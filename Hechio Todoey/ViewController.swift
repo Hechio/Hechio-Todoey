1349f369d7f56c427c1ccdf1684f8034a17a51a7
Steve Hechio <stevehechio@gmail.com>
2020-09-08 14:54:58 +0300

    Initial Commit

---
+++ Hechio Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hechio Todoey
+//
+//  Created by Joel Personal on 9/8/20.
+//  Copyright © 2020 Steve Hechio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
